5754edf3a2e70619458c2d0d21a93a5e0d0c5f02
thomas40510 <thomasprevost85@gmail.com>
2018-05-04 16:17:37 +0200

    //TestLog - 04/05/2018 @ 16:15//
    Added :
    - [sc] shows internet connection status
    
    Fixed :
    -
    
    Other :
    - Updated buildTools and Gradle

--- saisiecavalerie/src/main/java/com/dev/cyka/saisiecavalerie/MainActivity.java
+++ saisiecavalerie/src/main/java/com/dev/cyka/saisiecavalerie/MainActivity.java
@@ -1,8 +1,13 @@
 package com.dev.cyka.saisiecavalerie;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
@@ -29,6 +34,26 @@ public class MainActivity extends AppCompatActivity {
         //showRenew();
         ((TextView) findViewById(R.id.versionName)).setText(BuildConfig.VERSION_NAME);
 
+        final TextView webstatus  = (TextView) findViewById(R.id.webstatus);
+
+        final Handler handler = new Handler();
+        final int delay = 5000; //milliseconds
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        handler.postDelayed(new Runnable(){
+            public void run(){
+                webstatus.setText("fetching...");
+                if (!hasInternetConnection(getApplicationContext())){
+                    webstatus.setText("Not working !");
+                }
+                else{
+                    webstatus.setText("fine ;)");
+                }
+                handler.postDelayed(this, delay);
+            }
+        }, delay);
+
     }
 
 
@@ -48,5 +73,37 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, addVac.class);
         startActivity(intent);
     }
+
+    /**
+    public boolean isInternetWorking() {
+        boolean success = false;
+        try {
+            URL url = new URL("https://google.com");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setConnectTimeout(1000);
+            connection.connect();
+            success = connection.getResponseCode() == 200;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return success;
+    }
+     */
+
+    public static boolean hasInternetConnection(final Context context) {
+        final ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork!=null && activeNetwork.isConnectedOrConnecting();
+        return isConnected;
+        /*
+        final Network network = connectivityManager.getActiveNetwork();
+        final NetworkCapabilities capabilities = connectivityManager
+                .getNetworkCapabilities(network);
+
+        return capabilities != null
+                && capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED);
+    */
+    }
+
 
 }
